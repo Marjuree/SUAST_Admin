e0b541478a92e5718076af9eddfa61fe57f7d506
Marjuree <124424945+Marjuree@users.noreply.github.com>
2025-04-08 15:02:14 +0800

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -42,7 +42,6 @@ $current_page = basename($_SERVER['PHP_SELF']);
                 ],
                 "Accounting" => [
                     ["link" => "../AdminAccounting/AccountingDashboard.php", "icon" => "fa-tachometer-alt", "label" => "Dashboard"],
-                    ["link" => "../AdminAccounting/permit.php", "icon" => "fa-file", "label" => "Permit Request"],
                     ["link" => "../AdminAccounting/clearance.php", "icon" => "fa-clipboard-check", "label" => "Clearance Request"],
                     ["link" => "../AdminAccounting/student_balances.php", "icon" => "fa-book", "label" => "Student Record"],
                     ["link" => "../AdminAccounting/announcement.php", "icon" => "fa-bullhorn", "label" => "Announcement"],
