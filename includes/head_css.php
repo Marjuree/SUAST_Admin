3ee19fffada9250f2627ce83a0723a6d9d63fee7
Marjuree <124424945+Marjuree@users.noreply.github.com>
2025-04-19 16:05:41 +0800

    Update head_css.php

--- includes/head_css.php
+++ includes/head_css.php
@@ -5,39 +5,30 @@
     <meta charset="UTF-8">
     <title>SUAST</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-
-    <!-- Bootstrap 3 CSS -->
+    <!-- bootstrap 3.0.2 -->
     <link href="../../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-
-    <!-- Font Awesome -->
+    <!-- font Awesome -->
     <link href="../../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-
     <!-- Ionicons -->
     <link href="../../css/ionicons.min.css" rel="stylesheet" type="text/css" />
-
-    <!-- Morris Chart CSS -->
     <link href="../../js/morris/morris-0.4.3.min.css" rel="stylesheet" type="text/css" />
-
-    <!-- AdminLTE Theme -->
+    <!-- Theme style -->
     <link href="../../css/AdminLTE.css" rel="stylesheet" type="text/css" />
 
-    <!-- DataTables -->
     <link href="../../css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
-
-    <!-- Select2 -->
     <link href="../../css/select2.css" rel="stylesheet" type="text/css" />
-
-    <!-- jQuery -->
     <script src="../../js/jquery-1.12.3.js" type="text/javascript"></script>
-
-    <!-- Bootstrap JS (required for navbar, modals, dropdowns) -->
-    <script src="../../js/bootstrap.min.js" type="text/javascript"></script>
 
     <!-- Custom CSS for Header -->
     <style>
         .navbar, .logo {
-            background: rgb(0, 59, 222) !important;
-            color: #2c3e50;
+            background: #002B5B !important; 
+            color: #2c3e50; 
+        }
+        .logo img{
+            position: relative;
+            height: 50px;
+            width: 50px;
         }
     </style>
 </head>
