1ecc9f82c7df15fda6ce1d94cd7c496f92893e5a
Marjuree <124424945+Marjuree@users.noreply.github.com>
2025-04-22 00:41:13 +0800

    Update function.php

--- application/AdminSUAST/function.php
+++ application/AdminSUAST/function.php
@@ -1,36 +1,38 @@
 <?php
 require_once "../../configuration/config.php";
 
-if ($_POST['action'] == "edit") {
-    $id = $_POST['id'];
-    $name = $_POST['exam_name'];
-    $date = $_POST['exam_date'];
-    $time = $_POST['exam_time'];
-    $subject = $_POST['subject'];
-    $room = $_POST['room'];
+// Update exam schedule
+if ($_POST['action'] === "edit") {
+    $id = mysqli_real_escape_string($con, $_POST['id']);
+    $name = mysqli_real_escape_string($con, $_POST['exam_name']);
+    $date = mysqli_real_escape_string($con, $_POST['exam_date']);
+    $time = mysqli_real_escape_string($con, $_POST['exam_time']);
+    $subject = mysqli_real_escape_string($con, $_POST['subject']);
+    $room = mysqli_real_escape_string($con, $_POST['room']);
 
-    mysqli_query($con, "UPDATE tbl_exam_schedule SET exam_name='$name', exam_date='$date', exam_time='$time', subject='$subject', room='$room' WHERE id='$id'");
+    mysqli_query($con, "UPDATE tbl_exam_schedule 
+        SET exam_name='$name', exam_date='$date', exam_time='$time', subject='$subject', room='$room' 
+        WHERE id='$id'");
+
     echo "Exam schedule updated successfully!";
+    exit();
 }
 
-if ($_POST['action'] == "delete") {
-    $id = $_POST['id'];
+// Delete exam schedule
+if ($_POST['action'] === "delete") {
+    $id = mysqli_real_escape_string($con, $_POST['id']);
     mysqli_query($con, "DELETE FROM tbl_exam_schedule WHERE id='$id'");
     echo "Exam schedule deleted successfully!";
+    exit();
+}
+
+// Toggle schedule status (Enable/Disable)
+if ($_POST['action'] === "toggle_status") {
+    $id = mysqli_real_escape_string($con, $_POST['id']);
+    $status = mysqli_real_escape_string($con, $_POST['status']);
+
+    mysqli_query($con, "UPDATE tbl_exam_schedule SET status='$status' WHERE id='$id'");
+    echo "Status updated to '$status'";
+    exit();
 }
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
